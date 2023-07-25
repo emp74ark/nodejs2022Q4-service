9baf8e3399f30aab286276ca0a5d31406ea0fd2f
emp74ark <emp74ark@gmail.com>
2023-07-25 12:41:20 +0200

    refactor: use env variables

---
+++ src/entities/constants.ts
@@ -0,0 +1,13 @@
+import * as dotenv from 'dotenv';
+import * as process from 'process';
+
+dotenv.config();
+
+export const PORT = Number(process.env.PORT) || 4000;
+export const CRYPT_SALT = Number(process.env.CRYPT_SALT) || 10;
+export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || 'secret123123';
+export const JWT_SECRET_REFRESH_KEY =
+  process.env.JWT_SECRET_REFRESH_KEY || 'secret123123';
+export const TOKEN_EXPIRE_TIME = process.env.TOKEN_EXPIRE_TIME || '1h';
+export const TOKEN_REFRESH_EXPIRE_TIME =
+  process.env.TOKEN_REFRESH_EXPIRE_TIME || '24h';
