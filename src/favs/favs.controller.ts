349b6753ce988a657cb6e65698047a8453f9f433
emp74ark <emp74ark@gmail.com>
2023-08-09 17:39:13 +0200

    feat: save favorites in postgresql

--- src/favs/favs.controller.ts
+++ src/favs/favs.controller.ts
@@ -11,43 +11,43 @@ import { FavsService } from './favs.service';
 
 @Controller('favs')
 export class FavsController {
-  constructor(private readonly favsService: FavsService) {}
+  constructor(private readonly favService: FavsService) {}
 
-  // @Get()
-  // findAll() {
-  //   return this.favsService.findAll();
-  // }
-  //
-  // @Post('/track/:id')
-  // addTrack(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
-  //   return this.favsService.addTrack(id);
-  // }
-  //
-  // @Delete('/track/:id')
-  // @HttpCode(204)
-  // removeTrack(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
-  //   return this.favsService.removeTrack(id);
-  // }
-  //
-  // @Post('/album/:id')
-  // addAlbum(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
-  //   return this.favsService.addAlbum(id);
-  // }
-  //
-  // @Delete('/album/:id')
-  // @HttpCode(204)
-  // removeAlbum(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
-  //   return this.favsService.removeAlbum(id);
-  // }
-  //
-  // @Post('/artist/:id')
-  // addArtist(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
-  //   return this.favsService.addArtist(id);
-  // }
-  //
-  // @Delete('/artist/:id')
-  // @HttpCode(204)
-  // removeArtist(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
-  //   return this.favsService.removeArtist(id);
-  // }
+  @Get()
+  findAll() {
+    return this.favService.findAll();
+  }
+
+  @Post('/track/:id')
+  addTrack(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    return this.favService.addTrack(id);
+  }
+
+  @Delete('/track/:id')
+  @HttpCode(204)
+  removeTrack(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    return this.favService.removeTrack(id);
+  }
+
+  @Post('/album/:id')
+  addAlbum(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    return this.favService.addAlbum(id);
+  }
+
+  @Delete('/album/:id')
+  @HttpCode(204)
+  removeAlbum(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    return this.favService.removeAlbum(id);
+  }
+
+  @Post('/artist/:id')
+  addArtist(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    return this.favService.addArtist(id);
+  }
+
+  @Delete('/artist/:id')
+  @HttpCode(204)
+  removeArtist(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    return this.favService.removeArtist(id);
+  }
 }
--- src/favs/favs.service.ts
+++ src/favs/favs.service.ts
@@ -5,71 +5,103 @@ import { DatabaseService } from '../database/database.service';
 export class FavsService {
   constructor(private dbService: DatabaseService) {}
 
-  // async findAll() {
-  //   const favs = await this.dbService.getAllFavorites();
-  //
-  //   // get all non-null tracks by id
-  //   const tracks = await Promise.all(
-  //     favs.tracks.map(async (id) => await this.dbService.getTrackById(id)),
-  //   ).then((result) => result.filter((el) => Boolean(el)));
-  //
-  //   // get all non-null albums by id
-  //   const albums = await Promise.all(
-  //     favs.albums.map(async (id) => await this.dbService.getAlbumById(id)),
-  //   ).then((result) => result.filter((el) => Boolean(el)));
-  //
-  //   // get all non-null artists by id
-  //   const artists = await Promise.all(
-  //     favs.artists.map(async (id) => await this.dbService.getArtistById(id)),
-  //   ).then((result) => result.filter((el) => Boolean(el)));
-  //   console.log(artists);
-  //
-  //   return {
-  //     tracks,
-  //     albums,
-  //     artists,
-  //   };
-  // }
-  //
-  // async addTrack(id: string) {
-  //   const track = await this.dbService.getTrackById(id);
-  //   if (track) {
-  //     this.dbService.addTrackToFavorites(id);
-  //     return this.dbService.getTrackById(id);
-  //   }
-  //   throw new UnprocessableEntityException();
-  // }
-  //
-  // removeTrack(id: string) {
-  //   this.dbService.removeTrackFromFavorites(id);
-  //   return `Track with id ${id} was removed from favorites`;
-  // }
-  //
-  // async addAlbum(id: string) {
-  //   const album = await this.dbService.getAlbumById(id);
-  //   if (album) {
-  //     this.dbService.addAlbumToFavorites(id);
-  //     return this.dbService.getAlbumById(id);
-  //   }
-  //   throw new UnprocessableEntityException();
-  // }
-  //
-  // removeAlbum(id: string) {
-  //   this.dbService.removeAlbumFromFavorites(id);
-  //   return `Album with id ${id} was removed from favorites`;
-  // }
-  //
-  // async addArtist(id: string) {
-  //   const artist = await this.dbService.getArtistById(id);
-  //   if (artist) {
-  //     this.dbService.addArtistToFavorites(id);
-  //     return this.dbService.getArtistById(id);
-  //   }
-  //   throw new UnprocessableEntityException();
-  // }
-  //
-  // removeArtist(id: string) {
-  //   this.dbService.removeArtistFromFavorites(id);
-  //   return `Artist with id ${id} was removed from favorites`;
-  // }
+  async sessionUser() {
+    return await this.dbService.user.findFirst({});
+  }
+
+  async findAll() {
+    const user = await this.sessionUser();
+    return {
+      tracks: user.favTrack,
+      albums: user.favAlbum,
+      artist: user.favArtist,
+    };
+  }
+
+  async addTrack(id: string) {
+    const user = await this.sessionUser();
+
+    const track = await this.dbService.track.findUnique({
+      where: { id },
+    });
+
+    if (track) {
+      return this.dbService.user.update({
+        where: { id: user.id },
+        data: { favTrack: { push: track.id } },
+        select: {
+          favTrack: true,
+        },
+      });
+    }
+
+    throw new UnprocessableEntityException();
+  }
+
+  async removeTrack(id: string) {
+    const user = await this.sessionUser();
+    const favTrack = user.favTrack.filter((track) => track !== id);
+    return this.dbService.user.update({
+      where: { id: user.id },
+      data: { ...user, favTrack },
+    });
+  }
+
+  async addAlbum(id: string) {
+    const user = await this.sessionUser();
+
+    const album = await this.dbService.album.findUnique({
+      where: { id },
+    });
+
+    if (album) {
+      return this.dbService.user.update({
+        where: { id: user.id },
+        data: { favAlbum: { push: album.id } },
+        select: {
+          favAlbum: true,
+        },
+      });
+    }
+
+    throw new UnprocessableEntityException();
+  }
+
+  async removeAlbum(id: string) {
+    const user = await this.sessionUser();
+    const favAlbum = user.favAlbum.filter((album) => album !== id);
+    return this.dbService.user.update({
+      where: { id: user.id },
+      data: { ...user, favAlbum },
+    });
+  }
+
+  async addArtist(id: string) {
+    const user = await this.sessionUser();
+
+    const artist = await this.dbService.artist.findUnique({
+      where: { id },
+    });
+
+    if (artist) {
+      return this.dbService.user.update({
+        where: { id: user.id },
+        data: { favArtist: { push: artist.id } },
+        select: {
+          favArtist: true,
+        },
+      });
+    }
+
+    throw new UnprocessableEntityException();
+  }
+
+  async removeArtist(id: string) {
+    const user = await this.sessionUser();
+    const favArtist = user.favArtist.filter((artist) => artist !== id);
+    return this.dbService.user.update({
+      where: { id: user.id },
+      data: { ...user, favArtist },
+    });
+  }
 }
